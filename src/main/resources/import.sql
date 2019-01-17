07314c36cd8c896caa65688b1385c28f193eb35e
llicat <llicat@example.com>
2019-01-17 15:50:06 +0800

    import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,3 @@
 -- 初始化数据 admin/123456
--- INSERT INTO `ywxt`.`users` (`username`, `password`, `role`) VALUES ('admin', 'c093d41ff0a36bd70c1daec90b724ced', 'admin');
+INSERT INTO `users` (`username`, `password`, `nickname`, `introduction`, `avatar`) VALUES ('admin', 'c093d41ff0a36bd70c1daec90b724ced', '超级管理员', '我是超级管理员', 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif');
+INSERT INTO `user_role` (`userId`, `listIndex`, `roleName`) VALUES (1, 0, 'admin');
