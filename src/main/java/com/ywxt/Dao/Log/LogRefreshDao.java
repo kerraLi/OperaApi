a894deb23a18d51de9cfca5e4c761fa044f34c55
llicat <llicat@example.com>
2019-03-22 00:47:06 +0800

    repair

---
+++ src/main/java/com/ywxt/Dao/Log/LogRefreshDao.java
@@ -0,0 +1,10 @@
+package com.ywxt.Dao.Log;
+
+import com.ywxt.Domain.Log.LogRefresh;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LogRefreshDao extends JpaRepository<LogRefresh, Integer> {
+
+    LogRefresh getFirstByTypeOrderByIdDesc(String type);
+
+}
