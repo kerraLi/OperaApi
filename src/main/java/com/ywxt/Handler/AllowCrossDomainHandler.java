52a243f43de6069df19e0d4845a1bb9f2a82831f
llicat <llicat@example.com>
2019-03-31 12:59:48 +0800

    allowcross

--- src/main/java/com/ywxt/Handler/AllowCrossDomainHandler.java
+++ src/main/java/com/ywxt/Handler/AllowCrossDomainHandler.java
@@ -2,12 +2,16 @@ package com.ywxt.Handler;
 
 import com.ywxt.Utils.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 // 处理跨域
 @Component
@@ -15,10 +19,15 @@ public class AllowCrossDomainHandler implements HandlerInterceptor {
 
     @Autowired
     private Environment env;
+    @Value("${allow.cross.url}")
+    private String domains;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        if (env.getProperty("spring.profiles.active") != null && !env.getProperty("spring.profiles.active").equals("prod")) {
+        String origin = request.getHeader("Origin");
+        Set<String> allowOrigins = new HashSet<String>(Arrays.asList(domains.split(",")));
+        if ((env.getProperty("spring.profiles.active") != null && !env.getProperty("spring.profiles.active").equals("prod"))
+                || allowOrigins.contains(origin)) {
             response.setHeader("Access-Control-Allow-Origin", "*");
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, PATCH");
             response.setHeader("Access-Control-Max-Age", "0");
